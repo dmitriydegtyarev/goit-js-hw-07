8e7ffd31d93e49b70ec2788f37366a5aaef7cfab
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-03-10 20:51:58 +0200

    finish task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,9 +1,15 @@
-import users from './users.js';
+const inputRef = document.querySelector('#name-input');
+const spanRef = document.querySelector('#name-output');
 
-const getUserWithEmail = (users, email) => {
-  const user = users.find(user => user.email === email);
-  return user;
+// console.log(inputRef);
+// console.log(spanRef);
+
+const changeSpanOutputText = (inputText) => {
+  if (inputText.currentTarget.value.length > 0) {
+    spanRef.textContent = inputText.currentTarget.value;
+  } else {
+    spanRef.textContent = 'незнакомец';
+  }
 };
 
-console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+inputRef.addEventListener("input", changeSpanOutputText);
